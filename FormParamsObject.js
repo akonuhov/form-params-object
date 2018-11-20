85cad55d51c143228becd3d255c6863aa65fdff8
a.konuhov <alexandrkonuhov@gmail.com>
2018-11-20 17:29:12 +0500

    Fix bug set and get method

--- FormParamsObject.js
+++ FormParamsObject.js
@@ -8,12 +8,13 @@ FormParamsObject.prototype.getObjectProperty = function (path) {
   var props = path.split('.');
   var res = this.params;
   for (var i = 0; i < props.length; i++) {
-    const prop = props[i];
-    if (!res[prop]) {
+    var prop = props[i];
+    if (typeof res[prop] === 'undefined') {
       return null;
     }
     res = res[prop];
   }
+
   return res;
 };
 FormParamsObject.prototype.setObjectProperty = function (path, val) {
@@ -24,8 +25,8 @@ FormParamsObject.prototype.setObjectProperty = function (path, val) {
   var lastProp = props.pop();
   var res = this.params;
   for (var i = 0; i < props.length; i++) {
-    const prop = props[i];
-    if (typeof res[prop] !== 'object') {
+    var prop = props[i];
+    if (typeof res[prop] !== 'object' || res[prop] === null) {
       res[prop] = {};
     }
     res = res[prop];
